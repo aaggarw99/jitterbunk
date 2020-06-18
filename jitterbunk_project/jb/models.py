599f1b0b507d2c29cb92d2f8a95bcb7004f5321e
Ashwin Aggarwal <aaggarw99@gmail.com>
2020-06-18 13:41:38 -0500

    documentation changes

--- jitterbunk_project/jb/models.py
+++ jitterbunk_project/jb/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 class UserProfile(models.Model):
+    """
+    UserProfile Object
+    - USER points to the auth.User object of a user
+    - PHOTO holds a link to a user's photo.
+    """
     user = models.OneToOneField(User)
     photo = models.CharField(max_length=200, default="", blank=True)
 
@@ -9,6 +14,11 @@ class UserProfile(models.Model):
         return self.user.first_name + " " + self.user.last_name
 
 class Bunk(models.Model):
+    """
+    Bunk Object
+    - FROM_USER, TO_USER fields to denote which users sent a bunk
+    - TIMESTAMP field denoting the time of when the bunk was sent
+    """
     from_user = models.OneToOneField(UserProfile, related_name="from_user")
     to_user = models.OneToOneField(UserProfile, related_name="to_user")
     timestamp = models.DateTimeField('bunking date')
